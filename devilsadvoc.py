632b82e982a9516e22c56617408a99fdfce61d60
Sreeram V Gopal <sreeramvg100@gmail.com>
2026-09-12 13:33:52 +0530

    first agent call

--- devilsadvoc.py
+++ devilsadvoc.py
@@ -26,14 +26,39 @@ def searchweb(query: str) -> str:
 #Package the tools into a list
 tools=[searchweb]
 
-prompts=ChatPromptTemplate.from_messages([
-    ("system", """You are an analytical Red Team Agent and ruthless Devil's Advocate.
+#agent fills the placeholders and reads this template when a new input is recieved or even multiple times in a single execution
+prompt=ChatPromptTemplate.from_messages([
+    #foundational system intruction about how it should behave
+    ("system", """You are an analytical Red Team Agent and ruthless Devil's Advocate.       
     Your mission is to rigorously challenge the user's opinions, technical assumptions, or startup ideas.
     Rules:
     1. Never blindly agree or validate the user's thesis.
     2. For any claim made by the user, use the `search_web` tool to find counter-data, failure case studies, bottlenecks, or opposing research.
     3. Synthesize the findings into a clear, sharp, evidence-backed counterargument. Citing specific stats or facts discovered during your search is mandatory."""),
-    ("placeholder","{chathistory}"),
-    ("human",{input}),
-    ("placeholder", "{agentscratchpad}"),
+    ("placeholder","{chathistory}"),        #holds prior conversation. chat history is injected into this slot
+    ("human",{input}),                      #user live message is injected to this place
+    ("placeholder", "{agentscratchpad}"),   #where LangChain writes the agent's internal monologue. the workflow and decision by agent is made here.
 ])
+
+#this tells the gemini ai what all tools are available and will be attached to our prompts
+agent=create_tool_calling_agent(llm, tools, prompt)
+
+#creating a runtime executor, verbose=True lets us watch the internal thinking process and tool selection
+agentexecutor=AgentExecutor(agent=agent,tools=tools,verbose=True)
+
+if __name__=="__main__":
+    print("--------Devils Advocate--------")
+
+    #the statement ai will oppose
+    thesis="Python is strictly the best programming language for all backend development."
+    print(f"User Claim: {thesis}\n")
+
+    result=agentexecutor.invoke({
+        "input": thesis,
+        "chathistory": []
+    })
+
+    print("\n--- Counterargument ---")
+    print(result["output"])
+
+
